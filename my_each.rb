925b00461bd7015cf5eccebb0fad7f4a67ac3ad2
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-22 21:18:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each (array)# put argument(s) here
 count = 0
-  while count < array.lenght
-puts array[count]
-count += 1
-  end
-  end
+    while count < array.lenght
+  puts array[count]
+  count += 1
+    end
+  
 end
